ea2a66ca9411f4a4b5dfd37b5b87ad37c97a9e53
quint-jensen-67 <misterqj@gmail.com>
2016-05-13 09:57:01 -0500

    initial commit

---
+++ SplitFile/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SplitFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSrcFile = new System.Windows.Forms.TextBox();
+            this.btnSelFile = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtChunkSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSrcFile
+            // 
+            this.txtSrcFile.Location = new System.Drawing.Point(119, 33);
+            this.txtSrcFile.Name = "txtSrcFile";
+            this.txtSrcFile.Size = new System.Drawing.Size(276, 20);
+            this.txtSrcFile.TabIndex = 0;
+            // 
+            // btnSelFile
+            // 
+            this.btnSelFile.Location = new System.Drawing.Point(26, 31);
+            this.btnSelFile.Name = "btnSelFile";
+            this.btnSelFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelFile.TabIndex = 1;
+            this.btnSelFile.Text = "Select File";
+            this.btnSelFile.UseVisualStyleBackColor = true;
+            this.btnSelFile.Click += new System.EventHandler(this.btnSelFile_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 113);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "First Line is Header";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txtChunkSize
+            // 
+            this.txtChunkSize.Location = new System.Drawing.Point(119, 75);
+            this.txtChunkSize.Name = "txtChunkSize";
+            this.txtChunkSize.Size = new System.Drawing.Size(100, 20);
+            this.txtChunkSize.TabIndex = 3;
+            this.txtChunkSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChunkSize_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lines Per File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Split";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(119, 152);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(310, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Location = new System.Drawing.Point(30, 217);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessages.Size = new System.Drawing.Size(399, 82);
+            this.txtMessages.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 326);
+            this.Controls.Add(this.txtMessages);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtChunkSize);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnSelFile);
+            this.Controls.Add(this.txtSrcFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSrcFile;
+        private System.Windows.Forms.Button btnSelFile;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtChunkSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtMessages;
+    }
+}
+
---
+++ SplitFile/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SplitFile
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+        long lineCountGlobal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bw = new BackgroundWorker();
+        }
+
+        private void btnSelFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fb = new OpenFileDialog();
+
+            DialogResult dr = fb.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                this.txtSrcFile.Text = fb.FileName;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.txtMessages.Text = "Counting lines in CSV File" + Environment.NewLine;
+            bw.DoWork += SplitFile;
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.WorkerSupportsCancellation = true;
+            bw.WorkerReportsProgress = true;
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            bw.RunWorkerAsync();
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState != null)
+            {
+                SplitFileMessage msg = (SplitFileMessage)e.UserState;
+
+                if (msg.MessageType == SplitFileMessage.MSG_TYPE_ERR)
+                {
+                    throw new Exception(msg.Message);
+                }
+                else
+                {
+                    this.txtMessages.Text += msg.Message + Environment.NewLine;
+                }
+            }
+
+            progressBar1.Value = e.ProgressPercentage;
+            progressBar1.Update();
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.txtMessages.Text += "Completed File Splitting Process" + Environment.NewLine + " Wrote " + lineCountGlobal.ToString() + "Lines";
+            MessageBox.Show("Completed File Splitting Process!");
+        }
+
+        public void SplitFile(object sender, DoWorkEventArgs e)
+        {
+            long lineNumberIn = 0;
+            long lineNumberOut = 0;
+            lineCountGlobal = 0;
+            long chunkSize = Convert.ToInt64(this.txtChunkSize.Text);
+            long chunkCount = 1;//counts the number of chunks or files written
+            string srcFileFull = this.txtSrcFile.Text;
+            long totalLines = CountSourceLines(srcFileFull);
+            string srcFileName = Path.GetFileNameWithoutExtension(srcFileFull);
+            bool includeHeader = this.checkBox1.Checked;
+            string headerLine = "";
+            string destDir = Directory.GetParent(srcFileFull) + "\\split\\";
+            string ext = Path.GetExtension(srcFileFull);
+
+            bw.ReportProgress(0,new SplitFileMessage(SplitFileMessage.MSG_TYPE_INFO,"Found " + totalLines.ToString() + Environment.NewLine));
+            if (!Directory.Exists(destDir))
+            {
+                Directory.CreateDirectory(destDir);
+            }
+            string outFile = destDir + srcFileName + "_chunk" + chunkCount.ToString() + ext;
+            TextWriter destWriter = null;
+
+            try
+            {
+               destWriter = new StreamWriter(outFile);
+            }
+            catch (Exception ex)
+            {
+                SplitFileMessage msg = new SplitFileMessage(SplitFileMessage.MSG_TYPE_ERR, ex.Message);
+                bw.ReportProgress(0, msg);
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(srcFileFull))
+                {
+                    string csvLine;
+                    while((csvLine = reader.ReadLine()) != null)
+                    {
+                        lineNumberIn++;
+                        if (lineNumberIn == 1 && includeHeader)
+                        {
+                            headerLine = csvLine;
+
+                            //writes header line out first time through
+                            destWriter.WriteLine(headerLine);
+                            lineNumberOut++;
+                            lineCountGlobal++;
+                        }
+
+                        if (lineNumberOut <= chunkSize)
+                        {
+                            destWriter.WriteLine(csvLine);
+                            lineNumberOut++;
+                            lineCountGlobal++;
+                        }
+                        else
+                        {
+                            destWriter.Flush();
+                            destWriter.Close();
+                            chunkCount++;
+                            outFile = destDir + srcFileName + "_chunk" + chunkCount.ToString() + ext;
+                            try
+                            {
+                                destWriter = new StreamWriter(outFile);
+                                lineNumberOut = 1;
+                            }
+                            catch (Exception ex)
+                            {
+                                SplitFileMessage msg = new SplitFileMessage(SplitFileMessage.MSG_TYPE_ERR, ex.Message);
+                                bw.ReportProgress(0, msg);
+                            }
+                            if (includeHeader)
+                            {
+                                destWriter.WriteLine(headerLine);
+                                lineNumberOut++;
+                                lineCountGlobal++;
+                            }
+                            destWriter.WriteLine(csvLine);
+                            lineNumberOut++;
+                            lineCountGlobal++;
+                            //update progress bar after each chunk is processed
+                            double pct = ((double)lineNumberIn / (double)totalLines) * 100;
+                            int displayPct = (int)Math.Round(pct, 0);
+                            bw.ReportProgress(displayPct);
+                            //System.Threading.Thread.Sleep(0);
+                        }
+                        if (bw.CancellationPending)
+                        {
+                            e.Cancel = true;
+                            destWriter.Flush();
+                            destWriter.Close();
+                            Application.Exit();
+                        }
+                    }
+                    destWriter.Flush();
+                    destWriter.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                SplitFileMessage msg = new SplitFileMessage(SplitFileMessage.MSG_TYPE_ERR, ex.Message);
+                bw.ReportProgress(0, msg);
+            }
+            bw.ReportProgress(100);
+        }
+
+        /// <summary>
+        /// filters key presses so only numbers are able to be entered
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtChunkSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            const char Delete = (char)8;
+            e.Handled = !Char.IsDigit(e.KeyChar) && e.KeyChar != Delete;
+        }
+
+        /// <summary>
+        /// Counts the number of lines in file before process. This should be a pretty fast process
+        /// even if the file has millions of lines.
+        /// </summary>
+        /// <param name="path">Full path to the source CSV file</param>
+        /// <returns></returns>
+        public long CountSourceLines(string path)
+        {
+            long count = 0;
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        count++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SplitFileMessage msg = new SplitFileMessage(SplitFileMessage.MSG_TYPE_ERR, ex.Message);
+                bw.ReportProgress(0, msg);
+            }
+            return count;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (bw.IsBusy)
+            {
+                bw.CancelAsync();
+            }
+
+            //give the backgroundworker a few ms to finish
+            while (bw.IsBusy)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+
+    public class SplitFileMessage
+    {
+        public const int MSG_TYPE_INFO = 1;
+        public const int MSG_TYPE_ERR = 2;
+
+        private int _MessageType;
+
+        public int MessageType
+        {
+            get { return _MessageType; }
+            set { _MessageType = value; }
+        }
+
+        private string _Message;
+
+        public string Message
+        {
+            get { return _Message; }
+            set { _Message = value; }
+        }
+
+        public SplitFileMessage()
+        {
+        }
+
+        public SplitFileMessage(int MessageType, string Message)
+        {
+            this.MessageType = MessageType;
+            this.Message = Message;
+        }
+    }
+}
